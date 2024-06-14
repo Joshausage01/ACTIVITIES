a6a29e25945bae57c05ef1d4cf7fe99ccde424b6
Joshausage01 <159090739+Joshausage01@users.noreply.github.com>
2024-06-14 18:33:46 +0800

    Add files via upload
    programs for AddDays and GenerationIdentifier activities

---
+++ AddDays/AddDays/Program.cs
@@ -0,0 +1,71 @@
+﻿/*
+ * Write a C# program that does the following:
+ *  - user will input number of days
+ *  - program will display the corresponding day
+ *  - program will ask the user to input another input of number to add to the day number
+ *  - program will compute and display the corresponding day
+*/
+
+using System;
+namespace AddDays
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input day number (1-7): ");
+            int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+            if (dayNumber <= 0 || dayNumber > 7)
+            {
+                Console.WriteLine("Invalid input.");
+                return;
+            }
+            string day1 = GetDays(dayNumber);
+            Console.WriteLine($"The day is {day1}");
+
+            Console.Write("Input number to add to day: ");
+            int newDay = Convert.ToInt32(Console.ReadLine());
+
+            FinalDayValidation(dayNumber, newDay);
+        }
+        public static void FinalDayValidation(int num1,int num2)
+        {
+            if (num2 <= 0 || num2 > 7)
+            {
+                Console.WriteLine("Invalid input.");
+                return;
+            }
+            int finalDay = SolveFinalDay(num1, num2);
+            string day2 = GetDays(finalDay);
+            Console.WriteLine($"The new day is {day2}");
+        }
+        public static string GetDays(int num1) 
+        {
+            switch (num1)
+            {
+                case 1:
+                    return "MONDAY";
+                case 2:
+                    return "TUESDAY";
+                case 3:
+                    return "WEDNESDAY";
+                case 4:
+                    return "THURSDAY";
+                case 5:
+                    return "FRIDAY";
+                case 6:
+                    return "SATURDAY";
+                case 7:
+                    return "SUNDAY";
+                default:
+                    return "Invalid input.";
+            }
+        }
+        public static int SolveFinalDay(int num1, int num2)
+        {
+            int finalDay = (num1 + num2) % 7;
+            return finalDay;
+        }
+    }
+}
---
+++ GenerationIdentifier/GenerationIdentifier/Program.cs
@@ -0,0 +1,134 @@
+﻿/* Practice 4
+ * Write a C# program that does the ff:
+ * - user will enter their year of birth
+ * - the program will calculate and display the age, generation and age
+*/
+using System;
+
+namespace GenerationIdentifier
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            char choice;
+            do
+            {
+                Console.Clear();
+                Procedures();
+                choice = LoopChoiceValidation();        //validation of user's choice
+            }
+            while (choice == 'Y' || choice == 'y');
+            Console.WriteLine("Finished");
+            Console.ReadKey();
+        }
+        public static void Procedures()
+        {
+            int birthYear = InputBirthYear();               //identifying the user's birthyear.
+            int age = CalculateAge(birthYear);              //calculate age.
+            string henerasyon = IdentifyGeneration(age);    //identifying the user's generation.
+            string stages = IdentifyingLifeStage(age);      //identifying the life stage the user is at.
+
+            Console.WriteLine($"If the birth year is {birthYear}, then the person would be {age} years old, {henerasyon}, and in their {stages} of life");
+        }
+        public static int InputBirthYear()
+        {
+            int birthYear;
+            Console.Write("Enter your year of birth: ");
+            while (!int.TryParse(Console.ReadLine(), out birthYear))    //input validation
+            {
+                Console.Clear();
+                Console.WriteLine("Invalid input.");
+                Console.Write("Enter your year of birth: ");
+            }
+            return birthYear;
+        }
+        public static int CalculateAge(int num1)
+        {
+            int age = 2024 - num1;
+            return age;
+        }
+        public static string IdentifyGeneration(int age)
+        {
+            string henerasyon = "";
+            if (age >= 79 && age <= 96)
+            {
+                henerasyon = "The Silent Generation";
+            }
+            else if (age >= 60 && age <= 78)
+            {
+                henerasyon = "Baby Boomers";
+            }
+            else if (age >= 44 && age <= 59)
+            {
+                henerasyon = "Gen X";
+            }
+            else if (age >= 28 && age <= 43)
+            {
+                henerasyon = "Millennials";
+            }
+            else if (age >= 12 && age <= 27)
+            {
+                henerasyon = "Gen Z";
+            }
+            else if (age > 0 && age <= 11)
+            {
+                henerasyon = "gen alpha";
+            }
+            return henerasyon;
+        }
+        public static string IdentifyingLifeStage(int age)
+        {
+            string stages = "";
+            if (age >= 0 && age <= 3)
+            {
+                stages = "Infant";
+            }
+            else if (age > 3 && age <= 5)
+            {
+                stages = "Early Childhood";
+            }
+            else if (age >= 6 && age <= 8)
+            {
+                stages = "Middle Childhood";
+            }
+            else if (age >= 9 && age <= 11)
+            {
+                stages = "Late Childhood";
+            }
+            else if (age >= 12 && age <= 20)
+            {
+                stages = "Adoloscence";
+            }
+            else if (age >= 21 && age <= 35)
+            {
+                stages = "Early Adulthood";
+            }
+            else if (age >= 36 && age <= 50)
+            {
+                stages = "Midlife";
+            }
+            else if (age >= 51 && age <= 80)
+            {
+                stages = "Mature Adulthood";
+            }
+            else if (age > 80)
+            {
+                stages = "Late Adulthood";
+            }
+            return stages;
+        }
+        public static char LoopChoiceValidation()
+        {
+            char choice;
+            Console.WriteLine("\nWould you like to use it again? [y/n]");
+            while (!char.TryParse(Console.ReadLine(),out choice) || choice != 'y' && choice != 'n' )
+            {
+                Console.Clear();
+                Console.WriteLine("Invalid input. Only input  [y] for yes or [n] for no");
+                Console.WriteLine("Would you like to use it again? y/n");
+            }
+            return choice;
+        }
+    }
+}
